9b19657fe6dcf458d88f07a2b0a39310e59455a0
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-30 14:35:16 -0400

    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,18 @@ import { useEffect, useState } from "react";
 import React from "react";
 
 function Home(props) {
-      // create state to hold about data
-  const [about, setAbout] = useState(null);
+//       // create state to hold about data
+//   const [about, setAbout] = useState(null);
 
-  // create function to make api call
-  const getAboutData = async () => {
-    // make api call and get response
-    const response = await fetch(props.URL + "about");
-    // turn response into javascript object
-    const data = await response.json();
-    // set the about state to the data
-    setAbout(data);
-  };
+//   // create function to make api call
+//   const getAboutData = async () => {
+//     // make api call and get response
+//     const response = await fetch(props.URL + "about");
+//     // turn response into javascript object
+//     const data = await response.json();
+//     // set the about state to the data
+//     setAbout(data);
+//   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
   useEffect(() => getAboutData(), []);
@@ -21,9 +21,10 @@ function Home(props) {
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
     <div>
-      <h2>{about.name}</h2>
-      <h3>{about.email}</h3>
-      <p>{about.bio}</p>
+  <div className="introText">
+    <h1>Rosanne Anderson:</h1>
+    <h2>Software Engineer - Web Developer - Problem Solver</h2>
+  </div>
     </div>
   );
 
