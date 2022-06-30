8bc6259ca8fdf5d65ef3731f62256d05964b131a
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-30 14:19:26 -0400

    Netlify Deploy update remove header/footer imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 
 // IMPORT COMPONENTS
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 // IMPORT PAGES
 import Home from "./pages/Home";
